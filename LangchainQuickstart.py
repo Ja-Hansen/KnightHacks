65493efbb4f9268906b9f421d88e4fac66126429
Ja-Hansen <78921445+Ja-Hansen@users.noreply.github.com>
2023-10-07 14:23:38 -0400

    Delete LangchainQuickstart.py

--- LangchainQuickstart.py
+++
@@ -1,5 +0,0 @@
-from langchain.llms import OpenAI
-from langchain.chat_models import ChatOpenAI
-
-llm = OpenAI(openai_api_key="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")
-chat_model = ChatOpenAI(openai_api_key="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")
