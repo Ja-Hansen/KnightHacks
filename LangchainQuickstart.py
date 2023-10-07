52882437781bae703897dcddab41fb54055bb0f9
Ja-Hansen <jahansen1@usf.edu>
2023-10-07 14:22:20 -0400

    Create LangchainQuickstart.py

---
+++ LangchainQuickstart.py
@@ -0,0 +1,5 @@
+from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+
+llm = OpenAI(openai_api_key="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")
+chat_model = ChatOpenAI(openai_api_key="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")
