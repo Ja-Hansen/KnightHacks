3af574751e06846bd7ae762471e56316e1068c47
Ynot400 <147223483+Ynot400@users.noreply.github.com>
2023-10-08 01:50:04 -0400

    Im him

--- langchain_programming.py
+++ langchain_programming.py
@@ -2,7 +2,9 @@ from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 from langchain.chains import LLMChain
-
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+from langchain.agents import AgentType
 
 load_dotenv()
 
@@ -18,7 +20,20 @@ def generate_law(paperwork):
     response = client_chain({'paperwork': paperwork})
     return response
 
+def langchain_agent():
+    llm = OpenAI(temperature = 0)
+    tools = load_tools(["google-search"], llm = llm, google_cse_id="AIzaSyAb4woDoe0I8d1gpL1TrBf-Tc0abPMClak")
+    prompt_Template = PromptTemplate(
+        input_variables=[],
+        template="You are designed to be a friendly and consise chatbox that is under a company called Morgan&Morgan, which is Americas' largest law firm. You are required to answer questions from clients who need assistance with law firm proceedings."
+    )
+    agent = initialize_agent(
+        tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
+    )
+    question = "How challenging is a deposition?"
+    prompt = prompt_Template.format(question)
+    result = agent.run(prompt)
+    print(result)
 if __name__ == "__main__":
  
-   paper = input("Hello! Which document would you like to ")
-   print(generate_law("deposition"))
+    langchain_agent()
--- main2.py
+++ main2.py
@@ -1,9 +1,35 @@
 import langchain_programming as lcp
 import streamlit as lit
-
+import os
 lit.title("Morepain and Morepain")
-
-prompt1 = lit.sidebar.selectbox("Please choose the best option that matches your selection.", ("Add/View Documents", "Document Summarization", "General Inquriry", "Interrogatory Summarization"))
+user_documents = "user_documents"
+prompt1 = lit.sidebar.selectbox("Please choose the best option that matches your selection.", ("Add/View Documents", "Document Summarization", "General Inquiry", "Interrogatory Summarization"))
 
 if prompt1 == "Add/View Documents":
-    
+    semi_prompt1 = lit.sidebar.selectbox("Would you like to add or view a document?", ("Add", "View"))
+    if semi_prompt1 == "Add":
+        files = lit.file_uploader("Add Legal Documentation", type=["pdf"], accept_multiple_files=True, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible")
+        # Check if the folder exists; if not, create it
+        if not os.path.exists(user_documents):
+            os.mkdir(user_documents)
+        for file in files:
+            # Save the file to the "user_documents" folder
+            file_path = os.path.join(user_documents, file.name)
+            with open(file_path, "wb") as f:
+                f.write(file.read())
+            lit.write("Filename:", file.name)
+            lit.write("File has been accepted!")
+    else:
+        lit.subheader("PDF Documents currently submitted.")
+        # Get a list of PDF files in the user_documents folder
+        pdf_files = [f for f in os.listdir(user_documents) if f.endswith(".pdf")]
+
+        # Display the titles of PDF documents as clickable links
+        for pdf_file in pdf_files:
+            # Create a clickable link to open the PDF using HTML
+            pdf_link = f'<a href="{os.path.join(user_documents, pdf_file)}" target="_blank">{pdf_file}</a>'
+            lit.markdown(pdf_link, unsafe_allow_html=True)
+
+
+        
+        
